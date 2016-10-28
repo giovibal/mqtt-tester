e913dbf0ee67211c0081c3d65a1e07ba6507af69
Giovanni Baleani <giovibal@gmail.com>
2016-10-28 18:08:11 +0200

    Some renaming

--- cmd/mqtt-counter/main.go
+++ cmd/mqtt-counter/main.go
@@ -19,18 +19,22 @@ func main() {
 	flag.StringVar(&keepalive, "keepalive", "120", "keepalive, in seconds")
 	flag.StringVar(&username, "username", "", "username")
 	flag.StringVar(&password, "password", "", "password")
-	flag.StringVar(&topic, "topic", "mqtt-tester", "mqtt topic")
+	flag.StringVar(&topic, "topic", "#", "mqtt topic")
+	interval := flag.Duration("duration", 1*time.Second, "duration in seconds. " +
+		"Es. '300ms', '-1.5h' or '2h45m'). " +
+		"Valid time units are 'ns', 'us' (or 'µs'), 'ms', 's', 'm', 'h'.")
 	flag.Parse()
 
 	// TIMEOUT 10 sec
 	count := make(chan int64, 1)
 	go func() {
-		count <- tests.CountMessages(url, keepalive, username, password, topic, 1 * time.Second)
+		//count <- tests.CountMessages(url, keepalive, username, password, topic, 1 * time.Second)
+		count <- tests.CountMessages(url, keepalive, username, password, topic, *interval)
 	}()
 	select {
 	case ret := <-count:
 		fmt.Println(ret)
-	case <-time.After(10 * time.Second):
+	case <-time.After((10 * time.Second) + *interval):
 		fmt.Println("0")
 	}
 }
